c8338951a816dd794c5b50c2e8db0111a0d41051
Sunny Singh <KunwarSunnySingh.Rajput@Gmail.Com>
2021-03-29 16:19:43 +0530

    Dataset

--- Notebook.bat
+++
@@ -1,7 +0,0 @@
-::C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1
-::C:\Program Files\NVIDIA Corporation\NividiaGPUToolkitDevelopment\CUDA\v10.1\bin
-::SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.0\bin;%PATH%
-::SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.0\extras\CUPTI\lib64;%PATH%
-::SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.0\include;%PATH%
-::SET PATH=C:\tools\cuda\bin;%PATH%
-pyenv && jupyter notebook
